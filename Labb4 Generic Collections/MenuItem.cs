1996d01879dd429f05729c65fcc9bae3268196a1
darkdusk234 <johan.svensson.01@hotmail.com>
2024-11-11 12:48:57 +0100

    Created MenuItem class

---
+++ Labb4 Generic Collections/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Labb4_Generic_Collections
+{
+    internal class MenuItem
+    {
+
+        // I denna klass använder vi auto-implemented properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Price:C}";
+        }
+    }
+}
